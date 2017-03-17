70f558d23d59964e0b069664acfe6edf5de27081
carlosmienvio <carlos@mienvio.mx>
2017-03-16 20:16:15 -0600

    Changes-ancestor
    Change to add ancestors to each node.

--- bayes.py
+++ bayes.py
@@ -1,5 +1,6 @@
 import fileinput
 import json
+import copy
 import re
 
 
@@ -42,6 +43,7 @@ def read_input():
                         if prior[1:] not in net[c[1:]]['childs']:
                             net[c[1:]]['childs'].append(prior[1:])
                     # Save probability of event
+                    conds.sort()
                     net[prior[1:]][ (prior + '|' + ','.join(conds)) ] = prob
 
                 # Else probability is indepent, hence a root. Also calculate inverse probability
@@ -59,39 +61,134 @@ def read_input():
                 queries.append(line.strip())
     return net, queries, roots 
 
-def calculate_network(): 
-    pass
+# def child_snowball(ancestor, node):
+#     if ancestor in node[]
+
+def get_network_ancestors(net, roots): 
+    nodes = copy.deepcopy(roots)
+    visited = set()
+    str_node = nodes.pop()
+    node = net[str_node]
+    count = 0
+    # net[node]
+
+    while len(nodes) > 0:
+        if count < len(roots):
+            net[str_node]['ancestors'] = []
+        if 'childs' in node.keys():
+            for child in node['childs']:
+                if 'ancestors' not in net[child].keys():
+                    net[child]['ancestors'] = []
+                net[child]['ancestors'].extend(node['ancestors'])
+                valid = False
+                if 'parents' in net[child].keys():
+                    if any([True if p in visited else False for p in net[child]['parents']]):
+                        valid = True
+                if str(child) not in visited:
+                    valid = True
+                if valid:
+                    nodes.append(str(child))
+        visited.add(str(node))
+        str_node = nodes.pop()
+        node = net[str_node]
+
+    # for node in nodes:
+        # if 'childs' in net[node].keys():
+            # for child in net[root]['childs']:
+
+    # for node in net:
+    #     if 'parents' in net[node].keys():
+    #         for parent in net[node]['parents']:
+    #             neu_node = parent
+    #             while 'parents' in net[neu_node]:
+                    
+    #                 pass
+    #             print(parent)
+
+
+# def calculate_network(net, roots): 
+#     for node in net:
+#         if 'parents' in net[node].keys():
+#             for parent in net[node]['parents']:
+#                 neu_node = parent
+#                 while 'parents' in net[neu_node]:
+                    
+#                     pass
+#                 print(parent)
+#         # print(node)
+
+# def calculate_node(net, node):
+#     if 'parents' not in node.keys():
+#         return 
+    
+
+    # for parent in net[node]['parents']:
+    #     neu_node = parent
+    # if 'parents' in net[node].keys():
+    #             while 'parents' in net[neu_node]:
+                    
+    #                 pass
+    #             print(parent)
+
+def enumerate_algorithm(query):
+    result = ''
+    for q in query:
+        prior,cond = q.split('|')
+        print(prior,cond)
+
+    return result
+
+def clean_queries(qs):
+    neu_qs = []
+    for q in qs:
+        if '|' in q:
+            prior, cond = q.split('|')
+            conds = [c.strip() for c in cond.split(',')]
+            conds.sort()
+            priors = [p.strip() for p in prior.split(',')]
+            temp_priors = []
+            for p in priors:
+                temp_priors.append(p + '|' + ','.join(conds))
+            neu_qs.append(temp_priors)
+        else:
+            neu_qs.append([q.strip()])
+    return neu_qs
 
 def answer_queries(net,qs):
     answers = []
-    for query in qs:
-        if '|' in query:
-            prior, cond = query.split('|')
-            if query in net[prior[1:]].keys():
-                answers.append(net[prior[1:]][query])
+    for queries in qs:
+        for query in queries:
+            if '|' in query:
+                prior, cond = query.split('|')
+                if query in net[prior[1:]].keys():
+                    answers.append(net[prior[1:]][query])
+                else:
+                    answers.append('falta calcular')            
             else:
-                answers.append('falta calcular')            
-        else:
-            if query in net[query[1:]].keys():
-                answers.append(net[query[1:]][query])
-            else:
-                answers.append('falta calcular')            
-   return answers
+                if query in net[query[1:]].keys():
+                    answers.append(net[query[1:]][query])
+                else:
+                    answers.append('falta calcular')            
+    return answers
 
 
 def main():
 
     net, qs, roots = read_input()
 
-    calculate_network(net,roots)
+    # calculate_network(net,roots)
 
+    qs = clean_queries(qs)
     answers = answer_queries(net,qs)
 
-    print(roots)    
+    # print(roots)    
+    get_network_ancestors(net, roots)
     print(json.dumps(net,indent=2))    
-    
-    for a in answers: print(a)
+
+    # print(enumerate_algorithm(qs[4]))
+
+    # for a in answers: print(a)
 
 
-if __name__ == '__main__':
+if _name_ == '_main_':
     main()
